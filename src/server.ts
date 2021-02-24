52992452a6cbf881c4c1464eff54b6d642aa1352
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-02-24 00:16:48 -0300

    Second day of NLW04

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message : 'Hello World - NLW04'})
-})
-
-app.post('/', (request, response) => {
-  return response.json({ message : 'Data has been successfuly saved'})
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log('🖥️  Server is running on port 3333!'));

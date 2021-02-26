57a81ad3d1b41cfa8eb88987477f652526931d98
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-02-26 00:51:11 -0300

    Fourth day of NLW04

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import SurveysController from './controllers/SurveysController';
+import SendMailController from './controllers/SendMailController';
 import UsersController from './controllers/UsersController';
 
 const routes =  Router();
@@ -7,9 +8,13 @@ const routes =  Router();
 const usersController = new UsersController();
 const surveysController = new SurveysController();
 
+const sendMailController = new SendMailController();
+
 routes.post('/users', usersController.create);
 
 routes.post('/surveys', surveysController.create);
 routes.get('/surveys', surveysController.show);
+
+routes.post('/sendMail', sendMailController.execute)
 
 export default routes; 
